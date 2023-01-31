41686709344bede0be33b94cb48fdff441c504f9
gatari <79693291+deceptivecz@users.noreply.github.com>
2023-01-31 14:43:19 +0800

    formatting changes

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ from terminaltables import SingleTable
 from portscanner import Scanner
 from client import FTPHandler
 from custom_packet import PacketHandler
-clear = lambda: os.system('cls')
-data = [
-    ["1", "Port Scanner"], 
-    ["2", "FTP Client"], 
-    ["3", "Custom Packet"], 
-    ["4", "Exit"]
-]
-options_table: SingleTable = SingleTable(data)
-options_table.inner_row_border = 1
-while 1:
+def clear(): 
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def buffer():
+    input("Press enter to continue...")
+def main_menu():       
+    data = [
+        ["1", "Port Scanner"], 
+        ["2", "FTP Client"], 
+        ["3", "Custom Packet"], 
+        ["4", "Exit"]
+    ]
+    options_table: SingleTable = SingleTable(data)
+    options_table.inner_row_border = 1
     print(colored(
         rf"""
     ██████╗ ███████╗███████╗ ██████╗
@@ -28,28 +32,33 @@ while 1:
     {colored("Module Code: ", 'red', attrs=['bold'])} {colored("ST2414", 'blue', attrs=['bold'])}
     """, 'green').center(250))
     print(options_table.table)
-    choice = input("\t>> ")
+
+
+while 1:
+    clear()
+    main_menu()
+    choice = input(">> ")
     match choice:
         case '1':
             clear()
             # hosts = "localhost scanme.nmap.org"
-            options = "-sU -sT --top-ports 10 -sV -sC --traceroute -O"
+            # options = "-sU -sT --top-ports 10 -sV -sC --traceroute -O"
             hosts = "scanme.nmap.org"
-            # options = "-sU -sT --top-ports 10"
-            ps = Scanner(targets = hosts, options = options)
-            ps.run()
+            options = "-sU -sT --top-ports 10"
+            nmap_scan = Scanner(targets = hosts, options = options)
+            nmap_scan.run()
+            buffer()
         case '2':
             clear()
             IP = socket.gethostbyname(socket.gethostname())
             PORT = 2121
-            conn = FTPHandler(IP, PORT)
-            conn.run()
+            ftp_client = FTPHandler(IP, PORT)
+            ftp_client.run()
         case '3':
             clear()
-            ph = PacketHandler()
-            ph.run()
+            custom_packet = PacketHandler()
+            custom_packet.run()
         case '4':
             exit()
         case _:
             print("Invalid choice. Try again.")
-    input("Press enter to continue...")
